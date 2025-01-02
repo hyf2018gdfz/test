e0a6f885c9e0c3936545d85224de28495ea05539
Fmyh1828 <3256411401@qq.com>
2025-01-02 20:07:16 +0800

    ad1

---
+++ 123.cpp
@@ -0,0 +1,28 @@
+/*
+Author: hyf2018
+Time: 2025/01/02 20:07
+*/
+#include <bits/stdc++.h>
+#define lu unsigned long long
+#define ll long long
+#define iinf 0x3f3f3f3f
+#define linf 1e18
+#define N 300005
+#define M 500005
+#define mod 998244353
+// #define mod 1000000007
+#define uinv 281629680514649643
+// 131 * uinv % (2^64) == 1
+#define endl '\n'
+
+using namespace std;
+int n;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0), cout.tie(0);
+    cin >> n;
+    cout << n;
+
+    return 0;
+}
